f1d33c11a5e696e1c8b1cd87459f2dccbe443f02
zmjames <zmjames2000@163.com>
2019-09-25 00:19:36 +0800

    add two files rules.py and tools.py

---
+++ grammer/rules.py
@@ -0,0 +1,72 @@
+#encoding=utf-8
+
+import nltk,json
+from grammer.tools import ner_stanford, cut_stanford
+
+keep_pos="q,qg,qt,qv,s,t,tg,g,gb,gbc,gc,gg,gm,gp,m,mg,Mg,mq,n,an,vn,ude1,nr,ns,nt,nz,nb,nba,nbc,nbp,nf,ng,nh,nhd,o,nz,nx,ntu,nts,nto,nth,ntch,ntcf,ntcb,ntc,nt,nsf,ns,nrj,nrf,nr2,nr1,nr,nnt,nnd,nn,nmc,nm,nl,nit,nis,nic,ni,nhm,nhd"
+keep_pos_nouns=set(keep_pos.split(","))
+keep_pos_v="v,vd,vg,vf,vl,vshi,vyou,vx,vi"
+keep_pos_v=set(keep_pos_v.split(","))
+keep_pos_p ="p,pbei,pba"
+keep_pos_p=set(keep_pos_p.split(','))
+
+def get_stanford_ner_nodes(parent): #对date,number,... 进行识别
+    date=''
+    num=''
+    org=''
+    loc=''
+    for node in parent:
+        if type(node) is nltk.Tree:
+            if node.label() == 'DATE':
+                date = date + ' ' + ''.join([i[0] for i in node])
+            elif node.label() == 'NUMBER':
+                num = num + ' ' + ''.join([i[0] for i in node])
+            elif node.label() == 'ORGANIZATIONL':
+                org = org + " " + ''.join([i[0] for i in node])
+            elif node.label() == 'LOCATION':
+                loc = loc + " " + ''.join([i[0] for i in node])
+        if len(num) > 0 or len(date) > 0 or len(org) > 0 or len(loc) > 0:
+            return {'date': date, 'num': num, 'org': org, 'loc': loc}
+        else:
+            return {}
+
+
+def grammer_parse(raw_sentence=None, file_object=None):
+    # assert grammer_type in set(['hanlp_keep','stanford_ner_drop','stanford_pos_drop'])
+    if len(raw_sentence.strip()) < 1:
+        return False
+    grammer_dict = \
+        {
+
+            'stanford_ner_drop': r"""
+        DATE:{<DATE>+<MISC>?<DATE>*}
+        {<DATE>+<MISC>?<DATE>*}
+        {<DATE>+}
+        {<TIME>+}
+        ORGANIZATIONL:{<ORGANIZATION>+}
+        LOCATION:{<LOCATION|STATE_OR_PROVINCE|CITY|COUNTRY>+}
+        """
+        }
+
+    stanford_ner_drop_rp = nltk.RegexpParser(grammer_dict['stanford_ner_drop']) #解析语法
+    try:
+        stanford_ner_drop_result = stanford_ner_drop_rp.parse(ner_stanford(raw_sentence))
+        # 通过 Stanfordnlp的ner之后，再通过nltk的parse进行构建语法树
+
+    except:
+        print("the error sentence is {}".format(raw_sentence))
+    else:
+
+        stanford_keep_drop_dict = get_stanford_ner_nodes(stanford_ner_drop_result)
+        if len(stanford_keep_drop_dict) > 0:
+            file_object.write(json.dumps(stanford_keep_drop_dict, skipkeys=False,
+                                         ensure_ascii=False,
+                                         check_circular=True,
+                                         allow_nan=True,
+                                         cls=None,
+                                         indent=4,
+                                         separators=None,
+                                         default=None,
+                                         sort_keys=False))
+
+
---
+++ grammer/tools.py
@@ -0,0 +1,64 @@
+# encoding=utf-8
+
+import os,gc,re,sys
+from itertools import chain
+from stanfordcorenlp import StanfordCoreNLP
+import jpype
+
+def getJVMClass(packages):#JVM只需要调用一次就可以了
+    # 启动JVM
+    # print ('jvmPath:{}'.format(jpype.getDefaultJVMPath())) #jvmPath:D:\jdk-12.0.2\bin\server\jvm.dll
+    jvmPath = jpype.getDefaultJVMPath()#如果获取不到，可以用绝对路径，本地找一下
+    # 加载jar包，获取jar所在的路径
+    root_path = r'D:\Anaconda3\Lib\site-packages\pyhanlp\static'
+    djclass_path = "-Djava.class.path="+root_path+os.sep+'hanlp-1.7.4.jar;'+root_path
+    jpype.startJVM(jvmPath, djclass_path,"-Xms1g","-Xmx1g")
+    JDClass = jpype.JClass(packages)#包名.类名
+    # 创建类实例对象
+    jdc = JDClass()
+    return  jdc
+
+# Tokenizer = getJVMClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')
+# NLPTokenizer = getJVMClass('com.hankcs.hanlp.tokenizer.NLPTokenizer')
+# jpype.shutdownJVM()
+stanford_nlp = StanfordCoreNLP(r'D:\NLP_sourceCode\stanfordcorenlp\\', lang='zh')
+drop_pos_set = set(['xu','xx','y','yg','wh','wky','wkz','wp','ws','wyy','wyz','wb','u','ud','ude1','ude2','ude3','udeng','udh'])
+han_pattern=re.compile(r'[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]+')
+
+def to_string(sentence, return_generator=False):
+    if return_generator:
+        return (word_pos_item.toString().split('/') for word_pos_item in Tokenizer.segment(sentence))
+    else:
+        return [(word_pos_item.toString().split('/')[0], word_pos_item.toString().split('/')[1]) for word_pos_item in Tokenizer.segment(sentence)]
+        # return ' '.join([ word_pos_item.toString().split('/')[0] for word_pos_item in Tokenizer.segment(sentence)])
+    # 这里的“”.split('/')可以将string拆分成list 如：'ssfa/fsss'.split('/') => ['ssfa', 'fsss']
+
+def to_string_hanlp(sentence, return_generator=False):
+    if return_generator:
+        return ( word_pos_item.toString().split('/') for word_pos_item in Tokenizer.segment(sentence))
+    else:
+        return [(word_pos_item.toString().split('/')[0], word_pos_item.toString().split('/')[1]) for word_pos_item in Tokenizer.segment(sentence)]
+
+def seg_sentences(sentence, with_filter=True, return_generator=False):
+    segs = to_string(sentence, return_generator=return_generator)
+    if with_filter:
+        g = [word_pos_pair[0] for word_pos_pair in segs if len(word_pos_pair)==2 and word_pos_pair[0]!=' ' and word_pos_pair[1] not in drop_pos_set ]
+    else:
+        g = [word_pos_pair[0] for word_pos_pair in segs if len(word_pos_pair) == 2 and word_pos_pair[0] != ' ']
+    return iter(g) if return_generator else g
+
+def ner_stanford(raw_sentence, return_list=True):
+    if len(raw_sentence.strip())>0:
+        return stanford_nlp.ner(raw_sentence) if return_list else iter(stanford_nlp.ner(raw_sentence))
+
+def ner_handlp(raw_sentence, return_list=True):
+    if len(raw_sentence.strip())>0:
+        return NLPTokenizer.segment(raw_sentence) if return_list else iter(NLPTokenizer.segment(raw_sentence))
+
+def cut_stanford(raw_sentence, return_list=True):
+    if len(raw_sentence.strip())>0:
+        return stanford_nlp.pos_tag(raw_sentence) if return_list else iter(stanford_nlp.pos_tag(raw_sentence))
+
+def cut_hanlp(raw_sentence,return_list=True):
+    if len(raw_sentence.strip())>0:
+        return to_string(raw_sentence) if return_list else iter(to_string(raw_sentence))
