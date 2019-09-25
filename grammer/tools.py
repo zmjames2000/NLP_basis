c4d19f68c34dfbf73451ec3502c42ebd800869c9
zmjames <zmjames2000@163.com>
2019-09-25 14:18:12 +0800

    update more files

--- grammer/tools.py
+++ grammer/tools.py
@@ -10,17 +10,17 @@ def getJVMClass(packages):#JVM只需要调用一次就可以了
     # print ('jvmPath:{}'.format(jpype.getDefaultJVMPath())) #jvmPath:D:\jdk-12.0.2\bin\server\jvm.dll
     jvmPath = jpype.getDefaultJVMPath()#如果获取不到，可以用绝对路径，本地找一下
     # 加载jar包，获取jar所在的路径
+    # print(jvmPath)
     root_path = r'D:\Anaconda3\Lib\site-packages\pyhanlp\static'
     djclass_path = "-Djava.class.path="+root_path+os.sep+'hanlp-1.7.4.jar;'+root_path
-    jpype.startJVM(jvmPath, djclass_path,"-Xms1g","-Xmx1g")
+    jpype.startJVM(jvmPath, djclass_path) #D:\PyCharm\PyCharm 2019.2\bin\pycharm64.exe.vmoptions
     JDClass = jpype.JClass(packages)#包名.类名
     # 创建类实例对象
     jdc = JDClass()
     return  jdc
 
-# Tokenizer = getJVMClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')
+Tokenizer = getJVMClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')
 # NLPTokenizer = getJVMClass('com.hankcs.hanlp.tokenizer.NLPTokenizer')
-# jpype.shutdownJVM()
 stanford_nlp = StanfordCoreNLP(r'D:/NLP_sourceCode/stanfordcorenlp/', lang='zh')
 drop_pos_set = set(['xu','xx','y','yg','wh','wky','wkz','wp','ws','wyy','wyz','wb','u','ud','ude1','ude2','ude3','udeng','udh'])
 han_pattern=re.compile(r'[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]+')
